f354935715ed5ee3b0ce6b74884763e6fd842c89
okunishinishi <okunishinishi@gmail.com>
2012-11-04 15:35:50 +0900

    make move

--- js/index.js
+++ js/index.js
@@ -10,6 +10,29 @@
             card
                 .addClass(size % 2 == 0 ? 'even':'odd')
                 .addClass('card-' + size);
+            card.click(function(){
+                var style = (function(b, c){
+                    return {
+                        left:c.left - b.left - 5,
+                        top:c.top - b.top
+                    }
+
+                })($('body').offset(), card.offset());
+                var selected = card.clone()
+                    .insertAfter(card)
+                    .addClass('selected')
+                    .css(style)
+                    .animate({
+                        left:0,
+                        top:0,
+                        width:'100%',
+                        height:'100%',
+                        'font-size':'200px',
+                        'line-height':'320px',
+                        margin:0,
+                        'border-radius':20
+                    });
+            });
             return card;
         },
         cardChoiceSection:function(){
