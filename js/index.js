d4f893c9c3fc7e3a011b8570ae228bfd2f23e3c6
okunishinishi <okunishinishi@gmail.com>
2012-11-04 17:42:32 +0900

    add debug code

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-(function($){
+(function ($) {
     $.fn.extend({
-        clickOnTouchStart:function(){
+        clickOnTouchStart:function () {
             var elm = $(this);
-            elm.get(0).addEventListener('touchstart', function(e){
+            alert('elm.get(0)' + elm.get(0));
+            elm.get(0).addEventListener('touchstart', function (e) {
                 alert('e.touches.length = ' + e.touches.length);
-                if(e.touches.length === 1){
+                if (e.touches.length === 1) {
                     elm.trigger('click');
                     e.preventDefault();
                     e.stopPropagation();
@@ -13,7 +14,7 @@
             });
             return elm;
         },
-        card:function(value){
+        card:function (value) {
             var card = $(this)
                 .addClass('card');
             $('<span/>').addClass('number')
@@ -21,74 +22,74 @@
                 .text(value);
             var size = $('.card').size();
             card
-                .addClass(size % 2 == 0 ? 'even':'odd')
+                .addClass(size % 2 == 0 ? 'even' : 'odd')
                 .addClass('card-' + size);
             card
                 .clickOnTouchStart()
-                .click(function(){
-                if(card.is('.disabled')) return;
-                card.addClass('disabled');
-                var style = (function(b, co, c){
-                    return {
-                        left:c.left - co.left - 10,
-                        top:c.top - b.top
-                    }
-                })($('body').offset(), $('#content').offset(), card.offset());
+                .click(function () {
+                    if (card.is('.disabled')) return;
+                    card.addClass('disabled');
+                    var style = (function (b, co, c) {
+                        return {
+                            left:c.left - co.left - 10,
+                            top:c.top - b.top
+                        }
+                    })($('body').offset(), $('#content').offset(), card.offset());
 
-                var selected = card.clone()
-                    .insertAfter(card)
-                    .addClass('selected')
-                    .css(style)
-                    .animate({
-                        left:0,
-                        top:0,
-                        width:'100%',
-                        height:'100%',
-                        'font-size':'200px',
-                        'line-height':'380px',
-                        margin:0,
-                        'border-radius':20
-                    }, 500, null, function(){
-                        selected
-                            .clickOnTouchStart()
-                            .click(function(){
-                                if(closeBtn.is(':visible')){
-                                    closeBtn.hide();
-                                } else {
-                                    closeBtn.show();
+                    var selected = card.clone()
+                        .insertAfter(card)
+                        .addClass('selected')
+                        .css(style)
+                        .animate({
+                            left:0,
+                            top:0,
+                            width:'100%',
+                            height:'100%',
+                            'font-size':'200px',
+                            'line-height':'380px',
+                            margin:0,
+                            'border-radius':20
+                        }, 500, null, function () {
+                            selected
+                                .clickOnTouchStart()
+                                .click(function () {
+                                    if (closeBtn.is(':visible')) {
+                                        closeBtn.hide();
+                                    } else {
+                                        closeBtn.show();
+                                    }
+                                })
+                                .dblclick(function () {
+                                    closeBtn.trigger('click');
+                                });
+                            selected.get(0).addEventListener('touchstart', function (e) {
+                                if (e.touches.length > 1) {
+                                    selected.trigger('dblclick');
+                                    e.preventDefault();
+                                    e.stopPropagation();
+                                    e.stopImmediatePropagation();
                                 }
                             })
-                            .dblclick(function(){
-                                closeBtn.trigger('click');
-                            });
-                        selected.get(0).addEventListener('touchstart', function(e){
-                            if(e.touches.length > 1){
-                                selected.trigger('dblclick');
-                                e.preventDefault();
-                                e.stopPropagation();
-                                e.stopImmediatePropagation();
-                            }
-                        })
-                    });
-                var closeBtn = $('<a/>')
-                    .addClass('close-btn')
-                    .appendTo(selected)
-                    .text('×')
-                    .clickOnTouchStart()
-                    .one('click', function(){
-                        selected.fadeOut(300, function(){
-                            selected.remove();
-                            $('.selected').remove();
-                        })
-                    }).hide();
+                        });
+                    var closeBtn = $('<a/>')
+                        .addClass('close-btn')
+                        .appendTo(selected)
+                        .text('×')
+                        .clickOnTouchStart()
+                        .one('click',function () {
+                            selected.fadeOut(300, function () {
+                                selected.remove();
+                                $('.selected').remove();
+                            })
+                        }).hide();
                     card.removeClass('disabled');
-            });
+                });
             return card;
         },
-        cardChoiceSection:function(){
+        cardChoiceSection:function () {
             var section = $(this);
             var numbers = [0, 1];
-            while(numbers[1] < 13){
+            while (numbers[1] < 13) {
                 var number = numbers.shift() + numbers[0];
                 $('<div/>').card(number)
                     .appendTo(section);
@@ -98,9 +99,9 @@
             return section;
         }
     });
-    $(function(){
-        $('#card-choice-section').cardChoiceSection() ;
-        document.ontouchmove = function(event){
+    $(function () {
+        $('#card-choice-section').cardChoiceSection();
+        document.ontouchmove = function (event) {
             event.preventDefault();
         }
     });
